c355ddc88f85acf1324100e4619bf53d32c92f45
Anupam Basak <anupam.basak27@gmail.com>
2017-12-11 20:30:35 +0530

    Added View Controllers in Structure

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 #include <QQmlApplicationEngine>
 
 int main(int argc, char* argv[]) {
+  const char* uri = "org.nx.softwareupdater";
+
   QGuiApplication app(argc, argv);
-  QCoreApplication::addLibraryPath("./");
   QQmlApplicationEngine engine;
+
+  QCoreApplication::addLibraryPath("./");
+  QCoreApplication::setOrganizationName("NXOS");
+  QCoreApplication::setOrganizationDomain("nxos.org");
+  QCoreApplication::setApplicationName("nx-software-updater");
 
   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
--- src/ui/qml/main.qml
+++ src/ui/qml/main.qml
@@ -1,9 +1,9 @@
-import QtQuick 2.9
+import QtQuick 2.7
 import QtQuick.Controls 2.2
 
 ApplicationWindow {
     visible: true
     width: 600
-    height: 400
+    height: 250
     title: qsTr("NX Software Updater")
 }
