a8221a98355f7a0ca604f8ba16cca930f791b9bb
Anupam Basak <anupam.basak27@gmail.com>
2018-03-28 20:39:59 +0530

    Fix

--- travis-build.sh
+++ travis-build.sh
@@ -8,7 +8,7 @@ echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main' | tee /etc/apt/so
 wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
 
 ### Install Dependencies
-apt-get -yes update
+apt-get --yes update
 apt-get --yes dist-upgrade
 apt-get --yes install devscripts lintian cmake extra-cmake-modules build-essential automake autotools-dev g++ debhelper qtbase5-dev qtdeclarative5-dev libkf5auth-dev libkf5coreaddons-dev libkf5kcmutils-dev libkf5i18n-dev libkf5plasma-dev libkf5kdelibs4support-dev libkf5declarative-dev
 
