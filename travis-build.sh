324ecf42ababab46dcc8bc4997df8ac2572450ad
Anupam Basak <anupam.basak27@gmail.com>
2018-03-28 19:43:01 +0530

    Optimize Build Dependencies

--- travis-build.sh
+++ travis-build.sh
@@ -10,7 +10,7 @@ wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
 ### Install Dependencies
 apt-get -yes update
 apt-get --yes dist-upgrade
-apt-get --yes install cmake extra-cmake-modules build-essential automake autotools-dev g++ qtdeclarative5-dev-tools qml-module-qtgraphicaleffects plasma-framework-dev qtbase5-dev qtdeclarative5-dev libqt5xmlpatterns5-dev libkf5* gettext
+apt-get --yes install cmake extra-cmake-modules build-essential automake autotools-dev g++ debhelper qtbase5-dev qtdeclarative5-dev libkf5auth-dev libkf5coreaddons-dev libkf5kcmutils-dev libkf5i18n-dev libkf5plasma-dev libkf5kdelibs4support-dev libkf5declarative-dev
 
 ### Build Deb
 debuild -b -us -uc
